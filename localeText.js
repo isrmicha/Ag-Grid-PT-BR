ebd16e6fcddbc24db16cb606abd8226830167486
Israel Neves Micha <isrmicha@gmail.com>
2018-05-11 09:20:59 -0300

    Create localeText.js

---
+++ localeText.js
@@ -0,0 +1,87 @@
+let localeText = {
+      // for filter panel
+      page: 'Página',
+      more: 'Mais',
+      to: 'Para',
+      of: 'Do',
+      next: 'P´roximo',
+      last: 'daLasten',
+      first: 'daFirsten',
+      previous: 'daPreviousen',
+      loadingOoo: 'daLoading...',
+
+      // for set filter
+      selectAll: 'daSelect Allen',
+      searchOoo: 'daSearch...',
+      blanks: 'daBlanc',
+
+      // for number filter and text filter
+      filterOoo: 'daFilter...',
+      applyFilter: 'daApplyFilter...',
+
+      // for number filter
+      equals: 'daEquals',
+      lessThan: 'daLessThan',
+      greaterThan: 'daGreaterThan',
+
+      // for text filter
+      contains: 'daContains',
+      startsWith: 'daStarts dawith',
+      endsWith: 'daEnds dawith',
+
+      // the header of the default group column
+      group: 'laGroup',
+
+      // tool panel
+      columns: 'Colunas',
+      rowGroupColumns: 'laPivot Cols',
+      rowGroupColumnsEmptyMessage: 'la drag cols to group',
+      valueColumns: 'laValue Cols',
+      pivotMode: 'laPivot-Mode',
+      groups: 'laGroups',
+      values: 'laValues',
+      pivots: 'laPivots',
+      valueColumnsEmptyMessage: 'la drag cols to aggregate',
+      pivotColumnsEmptyMessage: 'la drag here to pivot',
+      toolPanelButton: 'Botão de painel de ferramentas',
+
+      // other
+      noRowsToShow: 'Não há registros para mostrar.',
+
+      // enterprise menu
+      pinColumn: 'Pinar coluna',
+      valueAggregation: 'Agregar valor',
+      autosizeThiscolumn: 'Redimensionar esta coluna',
+      autosizeAllColumns: 'Redimensionar todas colunas',
+      groupBy: 'Agrupar por',
+      ungroupBy: 'Desagrupar por',
+      resetColumns: 'Resetar colunas',
+      expandAll: 'Expandir tudo',
+      collapseAll: 'Contrair tudo',
+      toolPanel: 'Painel de ferramentas',
+      export: 'Exportar',
+      csvExport: 'Exportar para CSV',
+      excelExport: 'Exportar para Excel',
+
+      // enterprise menu pinning
+      pinLeft: 'Pinar <<',
+      pinRight: 'Pinar >>',
+      noPin: 'Sem pinagem',
+
+      // enterprise menu aggregation and status panel
+      sum: 'Soma',
+      min: 'Mínimo',
+      max: 'Máximo',
+      first: 'Primeiro',
+      last: 'Último',
+      none: 'Nenhum',
+      count: 'Contagem',
+      average: 'Média',
+
+      // standard menu
+      copy: 'Copiar',
+      copyWithHeaders: 'Copiar com cabeçalho',
+      ctrlC: 'Ctrl+C',
+      paste: 'Colar',
+      ctrlV: 'Ctrl+V'
+    }
